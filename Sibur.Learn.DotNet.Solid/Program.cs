501718ea339fcb4583fdcc590af46daa28008e42
abakumov-v <6594815+abakumov-v@users.noreply.github.com>
2021-01-26 10:09:08 +0300

    Add project files.

---
+++ Sibur.Learn.DotNet.Solid/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sibur.Learn.DotNet.Solid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
