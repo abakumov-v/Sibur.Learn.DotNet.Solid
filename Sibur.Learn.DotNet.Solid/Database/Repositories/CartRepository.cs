11d3ed2e844104b40c14e66dcfb7d8ba42383d72
abakumov-v <6594815+abakumov-v@users.noreply.github.com>
2021-01-26 10:29:41 +0300

    OCP - add fix for bad code

---
+++ Sibur.Learn.DotNet.Solid/Database/Repositories/CartRepository.cs
@@ -0,0 +1,15 @@
+﻿using Sibur.Learn.DotNet.Solid.Database.Abstract;
+using Sibur.Learn.DotNet.Solid.Models;
+
+namespace Sibur.Learn.DotNet.Solid.Database.Repositories
+{
+    public class CartRepository
+    {
+        public void Save(Cart entity)
+        {
+            // специфические действия для Cart
+            // очень много кода
+            // ...
+        }
+    }
+}
---
+++ Sibur.Learn.DotNet.Solid/Database/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Sibur.Learn.DotNet.Solid.Database.Abstract;
+using Sibur.Learn.DotNet.Solid.Models;
+
+namespace Sibur.Learn.DotNet.Solid.Database.Repositories
+{
+    public class UserRepository
+    {
+        public void Save(User entity)
+        {
+            // специфические действия для User
+            // очень много кода
+            // ...
+        }
+    }
+}
